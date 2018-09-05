81d95fe0f4988d1fd890c1dd913570dc91fe20ad
Kenold Beauplan <bkenold@gmail.com>
2018-09-05 09:47:35 -0400

    remove document.write, use Doc method

--- js/app.js
+++ js/app.js
@@ -1,115 +1,88 @@
-function displayTokens() {
-
-    var theForm =
-        '<form name="formTokens">' +
-            '<div class="form-group">' +
-                '<label for="numberInput">Enter a number between 20 and 100</label>' +
-                '<input type="text" name="numberInput" class="form-control">' +
-            '</div>' +
-            '<div class="form-group">' +
-                '<button class="btn btn-primary" type="button" onclick="displayTokens()">Display Tokens</button>' +
-            '</div>' +
-        '</form>';
+function generateTokens() {
 
     /* store form input value in 'number' variable
         - formTokens is the form name
         - numberInput is the input name
     */
     var number = document.formTokens.numberInput.value;
+    
     var min = 20;
     var max = 100;
     
-    // store Bootstrap CSS and custom css
-    var cssBootstrap = '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.2/css/bootstrap.min.css">';
-    var cssCustom = '<link rel="stylesheet" href="css/style.css">';
+    var remainder;
+    var result;
 
-    // document.write was overwriting the content and CSS from index.htmlso I had to re-include it here
-    document.write('<html><head><title>Generated Tokens - Kenold Beauplan Lab 2 / CTS2852C</title>' + 
-        cssBootstrap + cssCustom + '</head><body>');
+    // check if number from form input is not a number
+    if (isNaN(number)) {
+        // call the errorAlert() function and pass a string to it
+        errorAlert('Only numbers are accepted! ');
 
-        var mainHeader = 
-            '<header class="main-header">' +
-            '<h1>Token Generator</h1>' +
-            '<p class="lead">This program generates tokens worth values of 20, 10, 5, 2 and 1.</p>' +
-            '</header>';
+        // check if number input is between min=20 and max=100
+    } else if ((number > min) && (number < max)) {
 
-        document.write(mainHeader);
+        /* store sentence in 'result' variable and 
+           keep concatenating with result += below
+        */
+        result = '<h1>You entered <span class="text-success">' + number + '</span>.</h1>' +
+            '<p>Here are your generated tokens:</p>';
 
-        document.write('<div class="container">');
-            
-            // check if number input is not an integer
-            if (isNaN(number)) {
-                errorAlert('Only numbers are accepted! ');
-            // check if number input is between min=20 and max=100
-            } else if ((number > min) && (number < max)) {
+        // Get Twenties
+        var numOfTwenties = Math.floor(number / 20);
+        result += '<li class="list-group-item">' + numOfTwenties + ' x 20 tokens</li>';
+        remainder = number - (numOfTwenties * 20);
 
-                document.write('<h1>You entered <span class="text-success">' + number + '</span>.</h1>' +
-                    '<p>Here are your generated tokens:</p>');
-                
-                document.write('<ul class="list-group list-group-flush">');                
-                
-                    // Get Twenties
-                    var numOfTwenties = Math.floor(number / 20);
-                    document.write('<li class="list-group-item">' + numOfTwenties + " x 20 tokens</li>");
-                    var remainder = number - (numOfTwenties * 20);
+        // Get Tens
+        if (remainder >= 10) {
+            var numOfTens = Math.floor(remainder / 10);
+            result += '<li class="list-group-item">' + numOfTens + ' x 10 tokens</li>';
+            remainder -= 10;
+        }
+        // Get fives
+        if (remainder >= 5) {
+            var numOfFives = Math.floor(remainder / 5);
+            result += '<li class="list-group-item">' + numOfFives + ' x 5 tokens</li>';
+            remainder -= 5;
+        }
+        // Get twos
+        if (remainder >= 2) {
+            var numOfTwos = Math.floor(remainder / 2);
+            result += '<li class="list-group-item">' + numOfTwos + ' x 2 tokens</li>';
+            remainder -= 2;
+        }
+        // Get ones
+        if (remainder >= 1) {
+            var numOfOnes = Math.floor(remainder / 1);
+            result += '<li class="list-group-item">' + numOfOnes + ' x 1 token</li>';
+        }
 
-                    // Get Tens
-                    if (remainder >= 10) {
-                        var numOfTens = Math.floor(remainder / 10);
-                        document.write('<li class="list-group-item">' + numOfTens + " x 10 tokens</li>");
-                        remainder -= 10;
-                    }
-                    // Get fives
-                    if (remainder >= 5) {
-                        var numOfFives = Math.floor(remainder / 5);
-                        document.write('<li class="list-group-item">' + numOfFives + " x 5 tokens</li>");
-                        remainder -= 5;
-                    }
-                    // Get twos
-                    if (remainder >= 2) {
-                        var numOfTwos = Math.floor(remainder / 2);
-                        document.write('<li class="list-group-item">' + numOfTwos + " x 2 tokens</li>");
-                        remainder -= 2;
-                    }
-                    // Get ones
-                    if (remainder >= 1) {
-                        var numOfOnes = Math.floor(remainder / 1);
-                        document.write('<li class="list-group-item">' + numOfOnes + " x 1 token</li>");
-                    }
-                document.write('</ul>');
-                
-                document.write('<h4 class="mt-4">make up a total of <span class="badge badge-success">' 
-                    + number + '</span> tokens.</h4>');
-                
+        result += '<h4 class="mt-4">make up a total of <span class="badge badge-success">' +
+            number + '</span> tokens.</h4>';
 
-                document.write('<br><button class="btn btn-primary" onClick="reloadPage()">Generate New Tokens</button>');
-            } else {
-                errorAlert();
-            } // end if
+        // clear out errorsPlaceholder div before displaying tokens
+        document.getElementById("errorsPlaceholder").innerHTML = "";
 
-            function errorAlert(message='') {                
-                var errorMessage = 
-                    '<div class="alert alert-danger mb-0" role="alert">' +
-                        '<h4>ERROR!</h4>' +
-                        '<p>' + message + 'Please enter a number between 20 and 100.</p>' +
-                        '<hr>' +
-                        // if user enters nothing, display "You entered nothing", else display the number.                        
-                        '<p>You entered <strong>' + ((number == "") ? "nothing" : number) + "</strong>.</p>" +
-                    '</div>';
-                document.write(errorMessage);
-                document.write('<br><button class="btn btn-primary" onClick="reloadPage()">Try again!</button>');
-            }
-                        
-        document.write('</div>'); // close container div
+        // inject final 'result' in tokensPlaceholder div from index.html
+        document.getElementById("tokensPlaceholder").innerHTML = result;
 
-        document.write('<footer class="main-footer"><p>Created by Kenold Beauplan for CTS2852C</p></footer>');
+    } else {
+        errorAlert();
+    } // end if
 
-    // added double slash to escape one slash 🤦‍
-    document.write("<//body><//html>");
+    function errorAlert(message = '') {
+        var errorMessage =
+            '<div class="alert alert-danger mb-0" role="alert">' +
+            '<h4>ERROR!</h4>' +
+            '<p>' + message + 'Please enter a number between 20 and 100.</p>' +
+            '<hr>' +
+            // if number fiedl is empty "", display "You entered nothing", else display the number.                        
+            '<p>You entered <strong>' + ((number == "") ? "nothing" : number) + "</strong>.</p>" +
+            '</div>';
 
-} // function
+        // clear out tokensPlaceholder div before displaying error
+        document.getElementById("tokensPlaceholder").innerHTML = "";
 
-function reloadPage() {
-    // reload the page
-    document.location.reload(true);
-}
+        // display error alert in errors div
+        document.getElementById("errorsPlaceholder").innerHTML = errorMessage;
+    } // end errorAlert function
+
+} // end displayTokens function
